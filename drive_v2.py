feee8debd988e5b6d1f4b7f71728423de1ce3941
blai0007 <blai0007@student.monash.edu>
2024-08-20 14:43:51 +1000

    Forwards AND backwards Update

--- drive_v2.py
+++ drive_v2.py
@@ -6,10 +6,14 @@ from time import sleep
 import pygame
 
 # Set Pins
-in1 = 23
-in2 = 24
-en = 25
-temp1=1
+in1_left = 23
+in2_left = 24
+en_left = 25
+
+in1_right = 19
+in2_right = 26
+en_right = 13
+
 
 # Initialise Pygame Module
 pygame.init()
@@ -20,15 +24,25 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Initialise Pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.setup(in1_left,GPIO.OUT)
+GPIO.setup(in2_left,GPIO.OUT)
+GPIO.setup(en_left,GPIO.OUT)
+
+GPIO.setup(in1_right,GPIO.OUT)
+GPIO.setup(in2_right,GPIO.OUT)
+GPIO.setup(en_right,GPIO.OUT)
+
+GPIO.output(in1_left,GPIO.LOW)
+GPIO.output(in2_left,GPIO.LOW)
+GPIO.output(in1_right,GPIO.LOW)
+GPIO.output(in2_right,GPIO.LOW)
+
+p_left=GPIO.PWM(en_left,1000)
+p_right=GPIO.PWM(en_right,1000)
 
 # Enable the Motor Drivers
-p.start(25)
+p_left.start(25)
+p_right.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -41,13 +55,17 @@ def update_keyboard():
 
         if event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_UP :
-                GPIO.output(in1,GPIO.HIGH)
-                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in1_left,GPIO.HIGH)
+                GPIO.output(in2_left,GPIO.LOW)
+                GPIO.output(in1_right,GPIO.HIGH)
+                GPIO.output(in2_right,GPIO.LOW)
                 print("forward")
 
             if event.key == pygame.K_DOWN : 
-                GPIO.output(in1,GPIO.LOW)
-                GPIO.output(in2,GPIO.HIGH)
+                GPIO.output(in1_left,GPIO.LOW)
+                GPIO.output(in2_left,GPIO.HIGH)
+                GPIO.output(in1_right,GPIO.LOW)
+                GPIO.output(in2_right,GPIO.HIGH)
                 print("BACKWARDS")
 
             if event.key == pygame.K_LEFT : 
