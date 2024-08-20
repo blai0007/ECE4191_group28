d8c622dbfda28ee2771c13524d7ea98ed6003741
blai0007 <blai0007@student.monash.edu>
2024-08-20 12:19:29 +1000

    Updates to drive.py

--- drive.py
+++ drive.py
@@ -1,44 +1,51 @@
 import pygame
-import Encoder.py
+# from Encoder.py import Encoder
 from gpiozero import Robot
-en1 = Encoder(8,10)
-en2 = Encoder(22,24)
+import time
+# en1 = Encoder(8,10)
+# en2 = Encoder(22,24)
 
-en1.get
+pygame.init()
+SCREEN_WIDTH =  600
+SCREEN_HEIGHT = 400
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# en1.get
 # Drive function
 def update_keyboard(robot):
     for event in pygame.event.get():
-        #FORWARD
-        if event.type == pygame.KEYUP and event.key == pygame.K_UP:
-            #STOP DRIVING ON RELEASE
-            robot.stop()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            #DRIVE ON PRESS
-            robot.forward()
-        #LEFT
-        if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
-            robot.stop()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            robot.left()
-        #RIGHT
-        if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
-            robot.stop()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            robot.right()
-        #BACKWARD
-        if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
-            robot.stop()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            robot.reverse()
-        #STOP DRIVING
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            robot.stop()
+        if event.type == pygame.quit : 
+            break
 
-def getDistance():
-    wheel_rad = 27 #mm
-    degPerTick = 360/40
-    en1_rotated = en1.getValue() * degPerTick # deg
-    en2_rotated = en2.getValue() * degPerTick # deg
-    en1_distance = wheel_rad * en1_rotated 
-    en2_distance = wheel_rad * en2_rotated
-    return en1_distance, en2_distance
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_UP :
+                robot.forward() 
+                print("Forwardss")
+
+            if event.key == pygame.K_DOWN : 
+                robot.backwards()
+                print("BACKWARDS")
+
+            if event.key == pygame.K_LEFT : 
+                print("LEFT")
+
+            if event.key == pygame.K_RIGHT : 
+                print("RIGHT")
+
+# def getDistance():
+#     wheel_rad = 27 #mm
+#     degPerTick = 360/40
+#     en1_rotated = en1.getValue() * degPerTick # deg
+#     en2_rotated = en2.getValue() * degPerTick # deg
+#     en1_distance = wheel_rad * en1_rotated 
+#     en2_distance = wheel_rad * en2_rotated
+#     return en1_distance, en2_distance
+
+robot_1 = Robot(left=(5,6), right=(19,26))    # change it to gpiozero
+print("Let's Go!")
+
+while (True):
+    # update_keyboard(robot_1)
+    robot_1.forward()
+    time.sleep(0.1)
