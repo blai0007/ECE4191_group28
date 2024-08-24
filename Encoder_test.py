eb7e31b378e8ce51ace348b919e9efaef831cc94
blai0007 <blai0007@student.monash.edu>
2024-08-24 15:43:00 +1000

    Encoder class

--- Encoder_test.py
+++ Encoder_test.py
@@ -5,6 +5,28 @@ from time import sleep
 clk_pin = 17  # Replace with your GPIO pin for A (CLK)
 dt_pin = 18   # Replace with your GPIO pin for B (DT)
 
+def check_encoder() : 
+    clk_state = GPIO.input(clk_pin)
+    dt_state = GPIO.input(dt_pin)
+
+    # If the previous CLK state differs from the current state, a step has been made
+    if clk_state != last_clk_state:
+        # Determine the direction based on the state of DT
+        if dt_state != clk_state:
+            encoder_value += 1
+            direction = "Clockwise"
+        else:
+            encoder_value -= 1
+            direction = "Counterclockwise"
+
+        print(f"Steps: {encoder_value}, Direction: {direction}")
+
+    # Update the last state
+    last_clk_state = clk_state
+
+    # Small delay to prevent CPU overuse
+    sleep(0.01)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
--- multiprocessing_vision_test.py
+++ multiprocessing_vision_test.py
@@ -16,72 +16,24 @@ center = None
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
-class Encoder:
+def check_encoder(clk_pin, dt_pin, last_clk_state, encoder_value) : 
+    clk_state = GPIO.input(clk_pin)
+    dt_state = GPIO.input(dt_pin)
 
-    def __init__(self, leftPin, rightPin, callback=None):
-        self.leftPin = leftPin
-        self.rightPin = rightPin
-        self.value = 0
-        self.state = '00'
-        self.direction = None
-        self.callback = callback
-        GPIO.setup(self.leftPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.rightPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.leftPin, GPIO.BOTH, callback=self.transitionOccurred)  
-        GPIO.add_event_detect(self.rightPin, GPIO.BOTH, callback=self.transitionOccurred)  
+    # If the previous CLK state differs from the current state, a step has been made
+    if clk_state != last_clk_state:
+        # Determine the direction based on the state of DT
+        if dt_state != clk_state:
+            encoder_value += 1
+            direction = "Clockwise"
+        else:
+            encoder_value -= 1
+            direction = "Counterclockwise"
 
-    def transitionOccurred(self, channel):
-        p1 = GPIO.input(self.leftPin)
-        p2 = GPIO.input(self.rightPin)
-        newState = "{}{}".format(p1, p2)
+        print(f"Steps: {encoder_value}, Direction: {direction}")
 
-        if self.state == "00": # Resting position
-            if newState == "01": # Turned right 1
-                self.direction = "R"
-            elif newState == "10": # Turned left 1
-                self.direction = "L"
+    return clk_state, encoder_value
 
-        elif self.state == "01": # R1 or L3 position
-            if newState == "11": # Turned right 1
-                self.direction = "R"
-            elif newState == "00": # Turned left 1
-                if self.direction == "L":
-                    self.value = self.value - 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
-
-        elif self.state == "10": # R3 or L1
-            if newState == "11": # Turned left 1
-                self.direction = "L"
-            elif newState == "00": # Turned right 1
-                if self.direction == "R":
-                    self.value = self.value + 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
-
-        else: # self.state == "11"
-            if newState == "01": # Turned left 1
-                self.direction = "L"
-            elif newState == "10": # Turned right 1
-                self.direction = "R"
-            elif newState == "00": # Skipped an intermediate 01 or 10 state, but if we know direction then a turn is complete
-                if self.direction == "L":
-                    self.value = self.value - 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
-                elif self.direction == "R":
-                    self.value = self.value + 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
-                
-        self.state = newState
-        
-
-    def getValue(self):
-        return self.value
-    
-    def get_distance(self): 
-        return self.value * 4.32 
 
 # Set Pins
 in1_left = 5 # 23
@@ -115,7 +67,13 @@ GPIO.output(in2_right,GPIO.LOW)
 p_left=GPIO.PWM(en_left,1000)
 p_right=GPIO.PWM(en_right,1000)
 
-e1 = Encoder(encoder1_left_pin, encoder1_right_pin)
+# ENCODER SETUP
+GPIO.setup(encoder1_left_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoder1_right_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Initialize variables
+last_clk_state = GPIO.input(encoder1_left_pin)
+encoder_value = 0
 # e2 = Encoder(encoder2_left_pin, encoder2_right_pin)
 
 
@@ -475,6 +433,8 @@ while True:
     print(f"Y : {Robot.y}")
     print(f"DEG : {Robot.deg}")
 
+    # Encoder Stuff
+    last_clk_state, encoder_value = check_encoder(encoder1_left_pin, encoder1_right_pin, last_clk_state, encoder_value) 
     draw_window(Robot)
     time.sleep(0.1)
         
