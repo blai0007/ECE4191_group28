1254b1b46d55c0f8491b5c5b645da6d507660e1a
blai0007 <blai0007@student.monash.edu>
2024-08-24 13:36:41 +1000

    Encoder Test

--- multiprocessing_vision_test.py
+++ multiprocessing_vision_test.py
@@ -14,6 +14,73 @@ import RPi.GPIO as GPIO
 
 center = None
 
+class Encoder:
+
+    def __init__(self, leftPin, rightPin, callback=None):
+        self.leftPin = leftPin
+        self.rightPin = rightPin
+        self.value = 0
+        self.state = '00'
+        self.direction = None
+        self.callback = callback
+        GPIO.setup(self.leftPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.rightPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.leftPin, GPIO.BOTH, callback=self.transitionOccurred)  
+        GPIO.add_event_detect(self.rightPin, GPIO.BOTH, callback=self.transitionOccurred)  
+
+    def transitionOccurred(self, channel):
+        p1 = GPIO.input(self.leftPin)
+        p2 = GPIO.input(self.rightPin)
+        newState = "{}{}".format(p1, p2)
+
+        if self.state == "00": # Resting position
+            if newState == "01": # Turned right 1
+                self.direction = "R"
+            elif newState == "10": # Turned left 1
+                self.direction = "L"
+
+        elif self.state == "01": # R1 or L3 position
+            if newState == "11": # Turned right 1
+                self.direction = "R"
+            elif newState == "00": # Turned left 1
+                if self.direction == "L":
+                    self.value = self.value - 1
+                    if self.callback is not None:
+                        self.callback(self.value, self.direction)
+
+        elif self.state == "10": # R3 or L1
+            if newState == "11": # Turned left 1
+                self.direction = "L"
+            elif newState == "00": # Turned right 1
+                if self.direction == "R":
+                    self.value = self.value + 1
+                    if self.callback is not None:
+                        self.callback(self.value, self.direction)
+
+        else: # self.state == "11"
+            if newState == "01": # Turned left 1
+                self.direction = "L"
+            elif newState == "10": # Turned right 1
+                self.direction = "R"
+            elif newState == "00": # Skipped an intermediate 01 or 10 state, but if we know direction then a turn is complete
+                if self.direction == "L":
+                    self.value = self.value - 1
+                    if self.callback is not None:
+                        self.callback(self.value, self.direction)
+                elif self.direction == "R":
+                    self.value = self.value + 1
+                    if self.callback is not None:
+                        self.callback(self.value, self.direction)
+                
+        self.state = newState
+        
+
+    def getValue(self):
+        return self.value
+    
+    def get_distance(self): 
+        return self.value * 4.32 
+
 # Set Pins
 in1_left = 5 # 23
 in2_left = 6 # 24
@@ -46,8 +113,8 @@ GPIO.output(in2_right,GPIO.LOW)
 p_left=GPIO.PWM(en_left,1000)
 p_right=GPIO.PWM(en_right,1000)
 
-# e1 = Encoder(encoder1_left_pin, encoder1_right_pin)
-# e2 = Encoder(encoder2_left_pin, encoder2_right_pin)
+e1 = Encoder(encoder1_left_pin, encoder1_right_pin)
+e2 = Encoder(encoder2_left_pin, encoder2_right_pin)
 
 
 # Enable the Motor Drivers
