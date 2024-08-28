ebe0e8b54102a407f5a31171cd1d39589d55d908
blai0007 <blai0007@student.monash.edu>
2024-08-28 20:40:35 +1000

    0.001

--- multiprocessing_vision_test_v4.py
+++ multiprocessing_vision_test_v4.py
@@ -189,16 +189,16 @@ def center_ball(Robot, going_back):
                 if x_coord < 250: #Ball is on left
                     print("On the Left")
                     drive_left(Robot)
-                    # time.sleep(0.1)
-                    # drive_stop()
+                    time.sleep(0.001)
+                    drive_stop()
                 if x_coord > 350: #Ball is on right
                     print("On the Right")
                     drive_right(Robot)
-                    # time.sleep(0.1)
-                    # drive_stop()
+                    time.sleep(0.001)
+                    drive_stop()
             else:
                 print("Ball is within 250-350 pixels")
-                drive_stop()
+                # drive_stop()
 
 def automatic_brightness_and_contrast(image, clip_hist_percent=25):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
