e1555342c009a458c03ce0ff38dc9998480f07b6
blai0007 <blai0007@student.monash.edu>
2024-08-28 15:03:10 +1000

    test_v4

--- multiprocessing_vision_test_v4.py
+++ multiprocessing_vision_test_v4.py
@@ -481,8 +481,8 @@ while True:
 
     localisation(Robot, e1, e2)
     draw_window(Robot)
-    print(f"E1 : {e1.getValue}")
-    print(f"E2 : {e2.getValue}")
+    print(f"E1 : {e1.getValue()}")
+    print(f"E2 : {e2.getValue()}")
     time.sleep(0.1)
         
 
