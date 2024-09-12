76a201ead2d6dc7da3d4b3bd6ee5d6db6c3c8494
blai0007 <blai0007@student.monash.edu>
2024-09-12 13:46:31 +1000

    x

--- PI_Controller.py
+++ PI_Controller.py
@@ -16,34 +16,34 @@ class PIController:
         output = p + i
         return output
     
-    def motor_setpoint(setpoint, actual, dt):
+    def motor_setpoint(self, setpoint, actual, dt):
         error = setpoint - actual
         # Convert to RPM
         error = error / (900/60)
 
-        controller = pi_controller.compute(error, dt)
+        controller = self.compute(error, dt)
 
         return controller
 
 
 # Initialize the PI controller with gains
-pi_controller = PIController(Kp=10, Ki=0.1)
+# pi_controller = PIController(Kp=10, Ki=0.1)
 
-while True:
-    # Setpoint
-    expected_rpm = 100 # EXPECTED SPEED OF MOTOR
-    expected_tick_per_sec = expected_rpm * (900/60)
+# while True:
+#     # Setpoint
+#     expected_rpm = 100 # EXPECTED SPEED OF MOTOR
+#     expected_tick_per_sec = expected_rpm * (900/60)
 
-    en1_ticks = 100
-    en2_ticks = 120
-    dt = 0.1
-    # Ticks per second
-    en1_speed = en1_ticks / dt
-    en2_speed = en2_ticks / dt
+#     en1_ticks = 100
+#     en2_ticks = 120
+#     dt = 0.1
+#     # Ticks per second
+#     en1_speed = en1_ticks / dt
+#     en2_speed = en2_ticks / dt
 
-    # Compute new speed clamped from 0-100
-    m1_speed = max(0, min(100, pi_controller.motor_setpoint(expected_tick_per_sec, en1_speed, dt)))
-    m2_speed = max(0, min(100, pi_controller.motor_setpoint(expected_tick_per_sec, en2_speed, dt)))
+#     # Compute new speed clamped from 0-100
+#     m1_speed = max(0, min(100, pi_controller.motor_setpoint(expected_tick_per_sec, en1_speed, dt)))
+#     m2_speed = max(0, min(100, pi_controller.motor_setpoint(expected_tick_per_sec, en2_speed, dt)))
 
     # HERE YOU WOULD SET MOTOR FUNCTION
     # set_motor(in1_left, in2_left, motor_num=0, direction=1, speed=set_speed(m1_speed))
