2d2ff201068739cfbadba2783a7b0c82450c8dd9
blai0007 <blai0007@student.monash.edu>
2024-10-02 19:08:55 +1000

    clean

--- encoder_test.py
+++ encoder_test.py
@@ -13,8 +13,6 @@ class Encoder:
         self.callback = callback
         self.rising_edges = 0
         self.falling_edges = 0
-        GPIO.cleanup()
-        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.leftPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.rightPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(self.leftPin, GPIO.BOTH, callback=self.transitionOccurred)  
@@ -98,41 +96,15 @@ class Encoder:
 
 GPIO.setmode(GPIO.BCM)
 
-# Set Pins
-in1_left = 5 # 23
-in2_left = 6 # 24
-# en_left =  11 #25                # Simulating encoder
-
-in1_right = 19
-in2_right = 26
-# en_right = 13               # simulating encoder
-
 encoder1_left_pin = 7 # 7
 encoder2_left_pin = 23
 encoder1_right_pin = 8
 encoder2_right_pin = 24
 
-left_speed = 75
-right_speed = 75
-prev_encoder1_value = 0
-prev_encoder2_value = 0
-
 # Initialise Pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1_left,GPIO.OUT)
-GPIO.setup(in2_left,GPIO.OUT)
-# GPIO.setup(en_left,GPIO.OUT)
 
-GPIO.setup(in1_right,GPIO.OUT)
-GPIO.setup(in2_right,GPIO.OUT)
-# GPIO.setup(en_right,GPIO.OUT)
-
-GPIO.output(in1_left,GPIO.LOW)
-GPIO.output(in2_left,GPIO.LOW)
-GPIO.output(in1_right,GPIO.LOW)
-GPIO.output(in2_right,GPIO.LOW)
-
-e1 = Encoder(encoder1_left_pin, encoder1_right_pin)
+# e1 = Encoder(encoder1_left_pin, encoder1_right_pin)
 GPIO.cleanup()
 
 # GPIO.add_event_detect(encoder1_left_pin, GPIO.BOTH, callback=self.transitionOccurred)  
