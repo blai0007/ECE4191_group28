41737efc55b8e57f32594780da079d3db7accd37
blai0007 <blai0007@student.monash.edu>
2024-10-03 11:56:17 +1000

    s

--- encoder_test.py
+++ encoder_test.py
@@ -12,7 +12,7 @@ encoder2_right_pin = 24
 # Initialise Pins
 GPIO.setmode(GPIO.BCM)
 
-e1 = Encoder(encoder1_left_pin, encoder1_right_pin)
+e1 = Encoder(encoder2_left_pin, encoder2_right_pin)
 GPIO.cleanup()
 
 # GPIO.add_event_detect(encoder1_left_pin, GPIO.BOTH, callback=self.transitionOccurred)  
