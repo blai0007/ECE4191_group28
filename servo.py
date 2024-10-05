0b3de0b0120433ceb38096457c4afcb5bdbece1b
dche0074 <dche0074@student.monash.edu>
2024-10-05 12:58:52 +1000

    d

--- servo.py
+++ servo.py
@@ -1,9 +1,11 @@
 from adafruit_servokit import ServoKit
-
+import time
 kit = ServoKit(channels=16)
 try: 
     kit.servo[4].angle = 0
     kit.servo[15].angle = 140
+    while True:
+        time.sleep(1)
 except KeyboardInterrupt:
     kit.servo[4].angle = 140
     kit.servo[15].angle = 0
