09787ad6dc4cf2132aa027382b0e661bcc628691
dche0074 <dche0074@student.monash.edu>
2024-10-05 13:48:30 +1000

    gate

--- servo.py
+++ servo.py
@@ -7,7 +7,7 @@ try:
     kit.servo[15].angle = 140
     # Gate
     kit.continuous_servo[5].throttle = 1
-    time.sleep(1)
+    time.sleep(0.25)
     kit.continuous_servo[5].throttle = 0
     while True:
         time.sleep(1)
