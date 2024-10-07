583ec878f8727e3a602df91a5e2212de3aac81b4
dche0074 <dche0074@student.monash.edu>
2024-10-07 15:27:36 +1100

    d

--- localisation_v5.py
+++ localisation_v5.py
@@ -212,7 +212,7 @@ def move_to_reverse(robot) :
         return 1
 
 def turn_to_target(robot) : 
-    threshold = 10
+    threshold = 20
     print(f"Turning to Target : {robot.x_target_pygame, robot.y_target_pygame}")
     distance_x = robot.x_pygame - robot.x_target_pygame
     distance_y = -(robot.y_pygame - robot.y_target_pygame)
