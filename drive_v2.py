cd53f6a7dc2dc30ebfb846522d3ca49965de9e13
dche0074 <dche0074@student.monash.edu>
2024-10-07 16:10:53 +1100

    f

--- drive_v2.py
+++ drive_v2.py
@@ -17,15 +17,15 @@ pi_controller = PIController(Kp=10, Ki=0.06)
 # Set Pins
 in1_left = 5 # 23
 in2_left = 6 # 24
-# en_left =  11 #25                # Simulating encoder
+en_left =  11 #25                # Simulating encoder
 
 in1_right = 19
 in2_right = 26
-# en_right = 13               # simulating encoder
+en_right = 13               # simulating encoder
 
-encoder1_left_pin = 7
+encoder1_left_pin = 25
 encoder2_left_pin = 23
-encoder1_right_pin = 8
+encoder1_right_pin = 16
 encoder2_right_pin = 24
 
 speed = 1 # throttle speed from 0 to 1
@@ -36,16 +36,16 @@ SCREEN_WIDTH =  600
 SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-GPIO.cleanup()
+
 # Initialise Pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1_left,GPIO.OUT)
 GPIO.setup(in2_left,GPIO.OUT)
-# GPIO.setup(en_left,GPIO.OUT)
+GPIO.setup(en_left,GPIO.OUT)
 
 GPIO.setup(in1_right,GPIO.OUT)
 GPIO.setup(in2_right,GPIO.OUT)
-# GPIO.setup(en_right,GPIO.OUT)
+GPIO.setup(en_right,GPIO.OUT)
 
 GPIO.output(in1_left,GPIO.LOW)
 GPIO.output(in2_left,GPIO.LOW)
