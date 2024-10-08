a1ae440bdb44546a7a73fc087de197e4b452a29c
dche0074 <dche0074@student.monash.edu>
2024-10-08 20:39:09 +1100

    a

--- calib_test.py
+++ calib_test.py
@@ -62,7 +62,7 @@ def drive_stop():
 def calc_ticks_per_iter(current, prev, dt):
     abs(current - prev) / dt
 
-pi_controller = PIController(Kp=0.009,Ki=0.3) #0.043
+pi_controller = PIController(Kp=0.008,Ki=0.2) #0.043
 
 e1 = RotaryEncoder(encoder1_left_pin, encoder1_right_pin, max_steps=100000000)
 e2 = RotaryEncoder(encoder2_left_pin, encoder2_right_pin, max_steps=100000000)
