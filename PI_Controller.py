c2913c303d0c864b7ce6119e0126d84ed1947fb5
dche0074 <dche0074@student.monash.edu>
2024-10-08 20:03:03 +1100

    f

--- PI_Controller.py
+++ PI_Controller.py
@@ -21,7 +21,7 @@ class PIController:
         # Convert to RPM
         # error = error / (900/60)
         print(f"rotation error = {error}")
-        controller = self.compute(error, dt) / ((170 * 10/12 * 1) * 900 / dt*60)
+        controller = self.compute(error, dt) / 360 #/ ((170 * 10/12 * 1) * 900 / dt*60)
         if controller > 1:
             controller = 1
         elif controller < 0:
