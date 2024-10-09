a1e9d7dd4740c2884c3745a8f763c39c40338d2f
dche0074 <dche0074@student.monash.edu>
2024-10-09 15:03:28 +1100

    d

--- servo.py
+++ servo.py
@@ -6,7 +6,7 @@ try:
     kit.servo[4].angle = 0
     kit.servo[15].angle = 140
     # Gate
-    kit.servo[5].angle = 0
+    kit.servo[5].angle = 90
     # kit.continuous_servo[5].throttle = 1
     # time.sleep(0.25)
     # kit.continuous_servo[5].throttle = 0
