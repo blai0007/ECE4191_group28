a1e93e634c727e0fbfdc66c38eacd2d61c6bfe09
dche0074 <dche0074@student.monash.edu>
2024-10-09 20:03:27 +1100

    d

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,9 +1,10 @@
 from gpiozero import DistanceSensor
 ultrasonic = DistanceSensor(echo=10, trigger=9, threshold_distance=0.5)
-
+def inRange():
+    print(f"In Range: {ultrasonic.distance}")
+def outRange():
+    print("Out of Range")
 while True:
-    print(ultrasonic.distance)
-    # ultrasonic.wait_for_in_range()
-    # print(f"In range: {ultrasonic.distance}")
-    # ultrasonic.wait_for_out_of_range()
-    # print("Out of range") 
+    # print(ultrasonic.distance)
+    ultrasonic.when_in_range() = inRange
+    ultrasonic.when_out_of_range() = outRange
