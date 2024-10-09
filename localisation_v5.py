d92f7a8f6998e24e6cb495871c113279f7aa3975
blai0007 <blai0007@student.monash.edu>
2024-10-09 15:23:32 +1100

    a

--- localisation_v5.py
+++ localisation_v5.py
@@ -121,11 +121,11 @@ class robot :
         self.cm_per_tick = 60 / 3300                                  # Nathan and Bryan checked this, measure again if unsure
         self.ticks_per_full_rotation = 3900                            # TODO : Change this after wheel calibration
         self.degrees_per_tick = 360 / self.ticks_per_full_rotation
-        self.degrees_per_tick_wheel = 360 / 4000    #900     
+        self.degrees_per_tick_wheel = 360 / 40000    #900     
 
         # WAITING TIME (DT)
-        self.drive_dt = 0.02
-        self.turning_dt = 0.02
+        self.drive_dt = 0.2
+        self.turning_dt = 0.2
         self.loop_dt = 0.001
 
         # SEARCH PATTERN
