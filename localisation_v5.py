f323a8e4c287fbc9ff7f5f726ec0ed5149f4b300
blai0007 <blai0007@student.monash.edu>
2024-10-10 07:58:42 +1100

    b

--- localisation_v5.py
+++ localisation_v5.py
@@ -660,7 +660,7 @@ try:
                         MOVING_TO_BALL = 1
                         TURN_TO_BALL = 0
                 
-                if MOVING_TO_BALL == 1 : 
+                elif MOVING_TO_BALL == 1 : 
                     print("SYSTEM ACKNOWLDGE : MOVING TO TENNIS BALL")
                     drive_forward(Robot)
                     TURN_TO_BALL = 1
