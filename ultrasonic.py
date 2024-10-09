b95540d182468581ab4a9a23c04c413d52cb1bea
dche0074 <dche0074@student.monash.edu>
2024-10-09 22:47:08 +1100

    fe

--- ultrasonic.py
+++ ultrasonic.py
@@ -56,6 +56,12 @@ def set_motor(in1, in2, motor_num, direction, speed):
     speed = set_speed(speed)
     pca.channels[motor_num].duty_cycle = speed
 
+def drive_stop():
+    GPIO.output(in1_left,GPIO.LOW)
+    GPIO.output(in2_left,GPIO.LOW)
+    GPIO.output(in1_right,GPIO.LOW)
+    GPIO.output(in2_right,GPIO.LOW) 
+
 # pi_controller = PIController(Kp=0.0003,Ki=0.01,Kd=0) #0.043
 
 #Setup of GPIO pins
@@ -92,43 +98,31 @@ ticks_right_prev = 0
 set_motor(in1_left, in2_left, motor_num=0, direction=0, speed=90)
 set_motor(in1_right, in2_right, motor_num=1, direction=0, speed=70)
 
-while True: 
+try: 
+    for i in range(2500):
     # Motor control logic
-    left_ticks_iter = e1.steps - ticks_left_prev
-    right_ticks_iter = e2.steps - ticks_right_prev
-    w_left = (left_ticks_iter / dt) * degrees_per_tick 
-    w_right = (right_ticks_iter / dt) * degrees_per_tick 
+        left_ticks_iter = e1.steps - ticks_left_prev
+        right_ticks_iter = e2.steps - ticks_right_prev
+        w_left = (left_ticks_iter / dt) * degrees_per_tick 
+        w_right = (right_ticks_iter / dt) * degrees_per_tick 
 
-    m1_speed = 95
-    m2_speed = max(0, min(100, pi_controller.motor_setpoint(w_right, w_left, dt)))
+        m1_speed = 95
+        m2_speed = max(0, min(100, pi_controller.motor_setpoint(w_right, w_left, dt)))
 
-    ticks_left_prev = e1.steps
-    ticks_right_prev = e2.steps
+        ticks_left_prev = e1.steps
+        ticks_right_prev = e2.steps
 
-    set_motor(in1_left, in2_left, motor_num=0, direction=0, speed=m1_speed)
-    set_motor(in1_right, in2_right, motor_num=1, direction=0, speed=m2_speed)
+        set_motor(in1_left, in2_left, motor_num=0, direction=0, speed=m1_speed)
+        set_motor(in1_right, in2_right, motor_num=1, direction=0, speed=m2_speed)
 
-    if ultrasonic.distance < 0.07:
-        break
+        if ultrasonic.distance < 0.07:
+            break
 
-    sleep(dt)  # Sleep for the specified time step
-    if ultrasonic.distance < 0.07:
-        break
+        sleep(dt)  # Sleep for the specified time step
 
-ultrasonic.wait_for_in_range()
+except KeyboardInterrupt:
+    drive_stop()
 
-print(f'distance before stopping is {ultrasonic.distance}')
-before_dis = ultrasonic.distance
-#Stop Motors
-GPIO.output(in1_left,GPIO.LOW)              
-GPIO.output(in2_left,GPIO.LOW)
-GPIO.output(in1_right,GPIO.LOW)
-GPIO.output(in2_right,GPIO.LOW)
-
-print(f'distance after stopping is {ultrasonic.distance}')
-after_dis = ultrasonic.distance
-cal = before_dis-after_dis
-print(f'difference in distance is {cal}')
-
+drive_stop()
 
     
