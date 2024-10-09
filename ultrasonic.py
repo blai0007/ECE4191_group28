5aa8df02913cf91b3dad84061c1ea37cd94114ef
dche0074 <dche0074@student.monash.edu>
2024-10-09 21:09:06 +1100

    faefa

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,21 +1,43 @@
 from gpiozero import DistanceSensor
 import RPi.GPIO as GPIO
 
+# Ultrasonic Pi Pins
 echo = 11
 trigger = 9
 
+# LEFT MOTOR - WHEELS
+in1_left = 5                
+in2_left = 6                
+
+# RIGHT MOTOR - WHEELS
+in1_right = 19                
+in2_right = 26
+
+#Setup of GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(echo,GPIO.IN)
 GPIO.setup(trigger,GPIO.OUT)
+GPIO.setup(in1_left,GPIO.OUT)
+GPIO.setup(in2_left,GPIO.OUT)
+GPIO.setup(in1_right,GPIO.OUT)
+GPIO.setup(in2_right,GPIO.OUT)
 
-ultrasonic = DistanceSensor(echo=echo,trigger=trigger )#, threshold_distance=0.5)
+ultrasonic = DistanceSensor(echo=echo,trigger=trigger,threshold_distance=0.05) 
 
-def inRange():
-    print(f"In Range: {ultrasonic.distance}")
-def outRange():
-    print("Out of Range")
+#Intialisation of Motors - Starting ON GOING BACKWARDS 
+GPIO.output(in1_left,GPIO.LOW)              
+GPIO.output(in2_left,GPIO.HIGH)
+GPIO.output(in1_right,GPIO.LOW)
+GPIO.output(in2_right,GPIO.HIGH)
 
 while True:
-    # print(ultrasonic.distance)
-    ultrasonic.when_in_range = inRange
-    ultrasonic.when_out_of_range = outRange
+    ultrasonic.wait_for_in_range()
+    print("In Range")
+    break
+    
+#Stop Motors
+GPIO.output(in1_left,GPIO.LOW)              
+GPIO.output(in2_left,GPIO.LOW)
+GPIO.output(in1_right,GPIO.LOW)
+GPIO.output(in2_right,GPIO.LOW)
+    
